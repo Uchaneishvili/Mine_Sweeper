24834b7e497b3de647eef578eab57e52ab833473
Giga Uchaneishvili <g.uchaneishvili@gmail.com>
2025-04-03 17:19:37 +0400

    buttons are added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,71 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { StyleSheet, View, Button } from 'react-native'
 
 export default function HomeScreen() {
+  const map = '? ? ? ? ? ? ? ? ? ? ? ? ? ? ? 0 ? ? ? ? ? ? ? ? ? ? ? ? ? ? 0 0 0 ? ? ?'
+  const n = 6
+  const mapArray = map.split(' ')
+  const layout: string[][] = []
+  let rowArray: string[] = []
+
+  for (let i = 0; i < mapArray.length / n; i++) {
+    for (let j = 0; j < n; j++) {
+      const index = i * n + j
+
+      rowArray.push(mapArray[index])
+    }
+
+    layout.push(rowArray)
+    rowArray = []
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    <View style={styles.container}>
+      {layout.map(
+        (item: string[], index: number) => (
+          console.log('item', item),
+          (
+            <View key={index} style={styles.innerView}>
+              <View style={styles.row}>
+                {item.map(
+                  (value: string, innerIndex: number) => (
+                    console.log('valuee', value),
+                    (
+                      <View key={innerIndex} style={styles.buttonContainer}>
+                        <Button key={innerIndex} title={value} />
+                      </View>
+                    )
+                  )
+                )}
+              </View>
+            </View>
+          )
+        )
+      )}
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    verticalAlign: 'middle',
+    alignItems: 'center'
+  },
+  innerView: {
+    height: 50
+  },
+  buttonContainer: {
+    display: 'flex',
     flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+    gap: 10
+  }
+})
+// }
