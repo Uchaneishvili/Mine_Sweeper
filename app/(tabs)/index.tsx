feec83c092863719d03de053894c88701d59f0fb
Giga Uchaneishvili <g.uchaneishvili@gmail.com>
2025-04-03 18:57:22 +0400

    header is in progress

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,6 +6,15 @@ export default function HomeScreen() {
   const map = '? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ?'
   const n = 6
 
+  const buttonValue = {
+    x: '💣',
+    '?': ' ',
+    '0': '0',
+    '1': '1',
+    '2': '2'
+  }
+
+  const [gameOver, setGameOver] = useState(false)
   const [layout, setLayout] = useState(() => {
     const mapArray = map.split(' ')
     const initialLayout: string[][] = []
@@ -27,35 +36,60 @@ export default function HomeScreen() {
   const open = (row: number, column: number) => {
     const newLayout = layout.map((rowArray) => [...rowArray])
 
-    newLayout[column][row] = realMapArray[column * n + row]
+    if (realMapArray[column * n + row] === 'x') {
+      setGameOver(true)
+      const revealedLayout = Array(realMapArray.length / n)
+        .fill(null)
+        .map((_, i) => realMapArray.slice(i * n, (i + 1) * n))
 
-    setLayout(newLayout)
+      setLayout(revealedLayout)
+    } else {
+      newLayout[column][row] = realMapArray[column * n + row]
+      setLayout(newLayout)
+    }
   }
 
   return (
     <View style={styles.container}>
-      {layout.map((item: string[], columnIndex: number) => (
-        <View key={columnIndex} style={styles.innerView}>
-          <View style={styles.row}>
-            {item.map((value: string, rowIndex: number) => (
-              <View key={rowIndex} style={styles.buttonContainer}>
-                <TouchableOpacity
-                  style={[
-                    styles.button,
-                    { backgroundColor: value === 'x' ? 'red' : value !== '?' ? 'green' : 'gray' }
-                  ]}
-                  key={rowIndex}
-                  onPress={() => {
-                    open(rowIndex, columnIndex)
-                  }}
-                >
-                  <Text style={styles.buttonText}>{value}</Text>
-                </TouchableOpacity>
-              </View>
-            ))}
+      <View style={styles.innerContainer}>
+        {gameOver && <Text style={styles.gameOverText}>Game Over</Text>}
+
+        <View style={styles.contentContainer}>
+          <View style={styles.headerContainer}>
+            <View style={styles.headerButton}>1</View>
+            <View style={styles.headerButton}>1</View>
+            <View style={styles.headerButton}>1</View>
           </View>
+
+          {layout.map((item: string[], columnIndex: number) => (
+            <View key={columnIndex} style={styles.innerView}>
+              <View style={styles.row}>
+                {item.map((value: string, rowIndex: number) => (
+                  <View key={rowIndex} style={styles.buttonContainer}>
+                    <TouchableOpacity
+                      style={[
+                        styles.button,
+                        {
+                          backgroundColor: value === 'x' ? 'red' : value !== '?' ? 'green' : 'gray'
+                        }
+                      ]}
+                      key={rowIndex}
+                      disabled={value !== '?'}
+                      onPress={() => {
+                        open(rowIndex, columnIndex)
+                      }}
+                    >
+                      <Text style={styles.buttonText}>
+                        {buttonValue[value as keyof typeof buttonValue]}
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+                ))}
+              </View>
+            </View>
+          ))}
         </View>
-      ))}
+      </View>
     </View>
   )
 }
@@ -64,14 +98,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%'
+  },
+  innerContainer: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center'
   },
   innerView: {
-    height: 50
+    backgroundColor: 'gray'
   },
   buttonContainer: {
-    width: 50,
-    height: 50,
     margin: 2,
     justifyContent: 'center',
     alignItems: 'center'
@@ -81,17 +120,44 @@ const styles = StyleSheet.create({
     height: 40,
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 5
+    borderWidth: 5,
+    borderBottomColor: 'black',
+    borderTopColor: 'white',
+    borderLeftColor: 'white',
+    borderRightColor: 'black'
   },
   row: {
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center',
-    gap: 10
+    alignItems: 'center'
   },
   buttonText: {
     fontSize: 24,
     fontWeight: 'bold',
     color: 'white'
+  },
+  gameOverText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'red'
+  },
+  headerContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'gray',
+    justifyContent: 'space-between',
+    textAlign: 'center',
+    borderWidth: 5,
+    borderTopColor: 'black',
+    borderLeftColor: 'black',
+    borderRightColor: 'white',
+    borderBottomColor: 'white'
+  },
+  headerButton: {
+    width: 85,
+    height: 88,
+    backgroundColor: 'gray',
+    justifyContent: 'center'
   }
 })
